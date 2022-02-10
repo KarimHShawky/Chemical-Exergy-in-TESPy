440cca2e0758499b9db7ab03e7d4809968c042eb
MH <mathias.hofmann@tu-berlin.de>
2022-02-10 20:52:55 +0100

    summenprodukt für elemente funktioniert, es fehlen noch die delta g werte, work in progress

--- ChemicalExergyStandardValues.py
+++ ChemicalExergyStandardValues.py
@@ -9,7 +9,7 @@ import re
 
 # Values of standard chemical exergies of the elements, adopted from
 # Szargut et al. [QUELLE]
-ChemExElements = {'Ag': 70.2, 'Al': 988.2, 'Ar': 11.69, 'As': 494.6, 'Au': 50.5, 'B': 628.5, 'Ba': 775.1, 'Be': 604.4,
+Chem_Ex_Elements = {'Ag': 70.2, 'Al': 988.2, 'Ar': 11.69, 'As': 494.6, 'Au': 50.5, 'B': 628.5, 'Ba': 775.1, 'Be': 604.4,
                   'Bi': 274.5, 'Br': 101.2, 'C': 410.26, 'Ca': 729.1, 'Cd': 293.8, 'Ce': 1054.6, 'Cl': 123.6, 'Co': 312,
                   'Cr': 584.3, 'Cs': 404.4, 'Cu': 134.2, 'D2': 263.8, 'Dy': 975.9, 'Er': 972.8, 'Eu': 1003.8,
                   'F2': 504.9, 'Fe': 374.3, 'Ga': 514.9, 'Gd': 969, 'Ge': 557.6, 'H2': 236.09, 'He': 30.37,
@@ -20,33 +20,38 @@ ChemExElements = {'Ag': 70.2, 'Al': 988.2, 'Ar': 11.69, 'As': 494.6, 'Au': 50.5,
                   'Ru': 318.6, 'S': 609.6, 'Sb': 438.1, 'Sc': 925.2, 'Se': 346.5, 'Si': 854.9, 'Sm': 993.6, 'Sn': 551.9,
                   'Sr': 749.8, 'Ta': 974, 'Tb': 998.4, 'Te': 329.2, 'Th': 1202.6, 'Ti': 907.2, 'Tl': 194.9, 'Tm': 951.7,
                   'U': 1196.6, 'V': 720.4, 'W': 827.5, 'Xe': 40.33, 'Y': 965.5, 'Yb': 944.3, 'Zn': 339.2,
-                  'Zr': 1083.4}  # kJ/mol
+                    'Zr': 1083.4}  # kJ/mol
 
 #delta G Werte hier listen
 
-#Funktion ChemExCalc hier einbauen
-
-
 # Über alle Coolprop Stoff iterieren
-for i in CP.FluidsList():
+#for i in CP.FluidsList():
     # 1. Schritt
     # chemische Formel des Fluids holen
     # Elemente extrahieren
-    print(CP.get_fluid_param_string(i, 'REFPROP_name'), re.findall(r'(\w+)_{(\d+)', CP.get_fluid_param_string(i, 'formula')))
+    #print(CP.get_fluid_param_string(i, 'REFPROP_name'), re.findall(r'(\w+)_{(\d+)', CP.get_fluid_param_string(i, 'formula')))
     # ggf speichern als Liste oder dict
 
     # 2. Schritt
     # Elemente der Liste durchgehen und Werte an ChemExCalc Funktion übergeben
     # es fehlt noch eine Liste mit den delta G Werten
 
+# einen bestimmten Stoff vorgeben
+Fluid_Name = 'methane'
 
+#Fluid_Name_Refprop = CP.get_fluid_param_string(Fluid_Name, 'REFPROP_name')
 
+Fluid_Elements = re.findall(r'(\w+)_{(\d+)', CP.get_fluid_param_string(Fluid_Name, 'formula'))
 
+Fluid_Chem_Ex = 0
 
+for i in Fluid_Elements:
+    if i[0] in ['D', 'F', 'H', 'N', 'O']:
+        Fluid_Chem_Ex += Chem_Ex_Elements.get(i[0] + "2") * int(i[1]) / 2
+    else:
+        Fluid_Chem_Ex += Chem_Ex_Elements.get(i[0]) * int(i[1])
 
-
-
-
+print(Fluid_Chem_Ex/(CP.PropsSI('M', Fluid_Name)))
 
 
 # #GoF in kJ/mol
