ea1d0204de351e2ed59b22e9d5a5ab5f9e02f8c5
Karim Shawky <94178518+KarimHShawky@users.noreply.github.com>
2022-02-24 18:06:55 +0100

    Update chemical_exergy.py
    Changed naming scheme, removed calc_nonreactive_exergy

--- chemical_exergy.py
+++ chemical_exergy.py
@@ -11,25 +11,7 @@ from libChemExAhrendts import Chem_Ex
 import CoolProp.CoolProp as CP
 import numpy as np
 
-R=8.314/1000
-
-
-def calc_nonreact_exergy(conn, p0, T0):
-    ex_nonreact=0
-    hsum=0
-    ssum=0
-    if 1 in conn.fluid.val.values():
-    
-        ex_nonreact=0
-    else: 
-        for key in conn.fluid.val:
-                hsum+= conn.fluid.val[key]*CPSI('H','T',T0,'P',p0, key)
-                ssum+= conn.fluid.val[key]*CPSI('S','T',T0,'P',p0, key)
-        #print(hsum)            
-        ex_nonreact= ((fp.h_mix_pT([0, p0, 0, conn.fluid.val], T0)-hsum)
-                    -T0*(fp.s_mix_pT([0, p0, 0, conn.fluid.val], T0)-ssum))
-       # print((fp.h_mix_pT([0, p0, 0, conn.fluid.val], T0)-hsum))
-    return ex_nonreact 
+R=8.314462618/1000
 
 
 
@@ -39,13 +21,6 @@ def mass_to_mol_comp(conn, Molmasssum):
         x[key]= (conn.fluid.val[key])/CPSI('M', key)*(Molmasssum)
           
     return x
-
-def mol_to_mass_comp(omega,x_dry,Molmasssum):
-    
-    for key in x_dry:
-        omega[key]=x_dry[key]*CPSI('M',key)/Molmasssum
-    return omega 
-
 
 
 def cond_check (key, p0, T0, Molmasssum, x):
@@ -78,7 +53,7 @@ def cond_check (key, p0, T0, Molmasssum, x):
                 
     return  x, x_cond ,Cond
 
-def calc_reactive_exergy(conn, p0, T0):
+def calc_chemical_exergy(conn, p0, T0):
     x=dict()
     Cond=dict()
     x_cond=dict()
@@ -138,25 +113,22 @@ def calc_reactive_exergy(conn, p0, T0):
                             y= [Chem_Ex[k][3] for k in a if k in Chem_Ex]
                             ex_react_dry+=(x1[key]*y[0]+T0*R*x1[key]*(np.log(x1[key])))
     
-    ex_react= ex_react_cond + (1-x2)*ex_react_dry
-    ex_react*= 1/Molmasssum
+    ex_chemical= ex_react_cond + (1-x2)*ex_react_dry
+    ex_chemical*= 1/Molmasssum
   
-    return ex_react
+    return ex_chemical
 
 
 def get_chemical_exergy(conn, p0, T0):
-    ex_nonreact=calc_nonreact_exergy(conn,p0, T0)
+    
     
    
     
-    ex_react=calc_reactive_exergy(conn, p0, T0)
+    ex_chemical=calc_chemical_exergy(conn, p0, T0)
     
-    Ex_react= ex_react* conn.m.val_SI
-    Ex_nonreact= ex_nonreact* conn.m.val_SI
-    
-    ex_chemical= ex_react+ex_nonreact
+   
     Ex_chemical= ex_chemical*conn.m.val_SI
-    return ex_chemical
+    return Ex_chemical, ex_chemical
     
 
 
